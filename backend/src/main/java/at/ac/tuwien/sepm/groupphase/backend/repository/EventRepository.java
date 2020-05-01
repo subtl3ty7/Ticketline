949c61a2d3520b17340f35a05109bc8774770f14
egedemirsoy <egedemirsoy@gmail.com>
2020-05-01 16:28:15 +0200

    EventRepository is created. Finding Top 10 events of Month is implemented.

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
@@ -0,0 +1,15 @@
+package at.ac.tuwien.sepm.groupphase.backend.repository;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findAllByStartsAtAfter(LocalDateTime time);
+
+}
