5f87c61c6d1be42932d9404e228f5c5cf21fd7f4
egedemirsoy <egedemirsoy@gmail.com>
2020-05-01 16:27:31 +0200

    Event Service Interface is created. Finding Top 10 events of Month is defined.

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/EventService.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.sepm.groupphase.backend.service;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
+
+import java.util.List;
+
+public interface EventService {
+
+    List<Event> findTop10EventsOfMonth();
+}
