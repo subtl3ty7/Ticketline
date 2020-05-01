1ae23f4e8f247466c4aea640cc0f4e315298a194
egedemirsoy <egedemirsoy@gmail.com>
2020-05-01 16:25:25 +0200

    Event Endpoint is created. Finding Top 10 events of Month is implemented.

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/EventEndpoint.java
@@ -0,0 +1,48 @@
+package at.ac.tuwien.sepm.groupphase.backend.endpoint;
+
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.SimpleEventDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.EventMapper;
+import at.ac.tuwien.sepm.groupphase.backend.service.EventService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/events")
+public class EventEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final EventService eventService;
+    private final EventMapper eventMapper;
+
+    @Autowired
+    public EventEndpoint(EventService eventService, EventMapper eventMapper) {
+        this.eventService = eventService;
+        this.eventMapper = eventMapper;
+    }
+
+    @GetMapping(value = "/top10")
+    @ApiOperation(
+        value = "Get Top 10 events",
+        notes = "Get Top 10 events without details")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Events are successfully retrieved"),
+        @ApiResponse(code = 404, message = "No Event is not found"),
+        @ApiResponse(code = 500, message = "Connection Refused"),
+    })
+    public ResponseEntity<List<SimpleEventDto>> findTop10EventsOfMonth() {
+        LOGGER.info("GET /api/v1/events/top10");
+        List<SimpleEventDto> result = eventMapper.eventToSimpleEventDto(eventService.findTop10EventsOfMonth());
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
