8e035b41d7941588be55278636b5d6b8b4c6c1ab
Richard Binder <e1425185@student.tuwien.ac.at>
2020-05-03 21:09:37 +0200

    [Issue #3] Fixed some errors that happened while merging.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserAttemptsRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserAttemptsRepository.java
@@ -3,7 +3,9 @@ package at.ac.tuwien.sepm.groupphase.backend.repository;
 import at.ac.tuwien.sepm.groupphase.backend.entity.AbstractUser;
 import at.ac.tuwien.sepm.groupphase.backend.entity.UserAttempts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserAttemptsRepository extends JpaRepository<UserAttempts, Long> {
     UserAttempts findUserAttemptsByEmail(String email);
 
