85632d3f95beed884635d585bc8f2642d85a8934
Nejra <e11719704@student.tuwien.ac.at>
2020-05-03 16:19:30 +0200

    [Task #5] Implemented UserDataGenerator for Administrators

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/UserDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/UserDataGenerator.java
@@ -1,9 +1,6 @@
 package at.ac.tuwien.sepm.groupphase.backend.datagenerator;
 
-import at.ac.tuwien.sepm.groupphase.backend.entity.AbstractUser;
-import at.ac.tuwien.sepm.groupphase.backend.entity.Customer;
-import at.ac.tuwien.sepm.groupphase.backend.entity.Message;
-import at.ac.tuwien.sepm.groupphase.backend.entity.UserAttempts;
+import at.ac.tuwien.sepm.groupphase.backend.entity.*;
 import at.ac.tuwien.sepm.groupphase.backend.repository.MessageRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.UserAttemptsRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.UserRepository;
@@ -22,9 +19,11 @@ import java.time.LocalDateTime;
 public class UserDataGenerator {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private static final int NUMBER_OF_CUSTOMERS_TO_GENERATE = 3;
-    private static final String TEST_USER_FIRST_NAME = "First Name";
-    private static final String TEST_USER_LAST_NAME = "Last Name";
-    private static final String TEST_USER_EMAIL = "E-Mail";
+    private static final int NUMBER_OF_ADMINISTRATORS_TO_GENERATE = 2;
+    private static final String TEST_USER_FIRST_NAME = "Name";
+    private static final String TEST_USER_LAST_NAME = "Surname";
+    private static final String TEST_CUSTOMER_EMAIL = "@customer.com";
+    private static final String TEST_ADMIN_EMAIL = "@admin.com";
     private static final String TEST_USER_PASSWORD = "Password";
     private static final String TEST_USER_CODE = "U123X";
 
@@ -46,10 +45,10 @@ public class UserDataGenerator {
             LOGGER.debug("generating {} customers", NUMBER_OF_CUSTOMERS_TO_GENERATE);
             for (int i = 0; i < NUMBER_OF_CUSTOMERS_TO_GENERATE; i++) {
                 AbstractUser customer = Customer.CustomerBuilder.aCustomer()
-                    .withFirstName(TEST_USER_FIRST_NAME + " " + i)
-                    .withLastName(TEST_USER_LAST_NAME + " " + i)
-                    .withEmail(TEST_USER_EMAIL + " " + i)
-                    .withPassword(passwordEncoder.encode(TEST_USER_PASSWORD + " " + i))
+                    .withFirstName(TEST_USER_FIRST_NAME  + i)
+                    .withLastName(TEST_USER_LAST_NAME  + i)
+                    .withEmail( "e" + i + TEST_CUSTOMER_EMAIL)
+                    .withPassword(passwordEncoder.encode(TEST_USER_PASSWORD + i))
                     .withUserCode(TEST_USER_CODE + i)
                     .withBirthday(LocalDateTime.now().minusMonths(i))
                     .withIsLogged(false)
@@ -60,13 +59,36 @@ public class UserDataGenerator {
                     .build();
                 UserAttempts attempts = UserAttempts.UserAttemptsBuilder.aAttempts()
                     .withAttempts(0)
-                    .withEmail(TEST_USER_EMAIL + " " + i)
+                    .withEmail("e" + i + TEST_CUSTOMER_EMAIL)
                     .build();
-                LOGGER.debug("saving customer " + customer.getFirstName() + " with attempts " + attempts.getAttempts() );
+                LOGGER.debug("saving customer " + customer.getEmail() + " with attempts " + attempts.getAttempts() );
                 userRepository.save(customer);
                 userAttemptsRepository.save(attempts);
             }
         }
     }
 
+    @PostConstruct
+    private void generateAdministrator() {
+        if (userRepository.findAll().size() > 3) {
+            LOGGER.debug("administrators already generated");
+        } else {
+            LOGGER.debug("generating {} administrators", NUMBER_OF_ADMINISTRATORS_TO_GENERATE);
+            for (int i = 0; i < NUMBER_OF_ADMINISTRATORS_TO_GENERATE; i++) {
+                AbstractUser administrator = Administrator.AdministratorBuilder.aAdministrator()
+                    .withFirstName(TEST_USER_FIRST_NAME  + i)
+                    .withLastName(TEST_USER_LAST_NAME  + i)
+                    .withEmail( "e" + i + TEST_ADMIN_EMAIL)
+                    .withPassword(passwordEncoder.encode(TEST_USER_PASSWORD + i))
+                    .withUserCode(TEST_USER_CODE + (9-i))
+                    .withBirthday(LocalDateTime.now().minusMonths(i))
+                    .withIsLogged(false)
+                    .withCreatedAt(LocalDateTime.now())
+                    .withUpdatedAt(LocalDateTime.now())
+                    .build();
+                LOGGER.debug("saving customer " + administrator.getEmail() );
+                userRepository.save(administrator);
+            }
+        }
+    }
 }
