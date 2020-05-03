65061d5d7f3a25d21917ed97c4bd23655527d320
Nejra <e11719704@student.tuwien.ac.at>
2020-05-03 17:00:05 +0200

    [Task #05] Frontend fixes for redirecting admins to an administration page

--- frontend/src/app/dtos/user.ts
+++ frontend/src/app/dtos/user.ts
@@ -1,19 +1,19 @@
 export class User {
   constructor(
     public id: number,
-    public user_code: string,
-    public first_name: string,
-    public last_name: string,
+    public userCode: string,
+    public firstName: string,
+    public lastName: string,
     public email: string,
     public password: string,
     public birthday: string,
-    public created_at: string,
-    public updated_at: string,
-    public is_blocked: boolean,
-    public is_logged: boolean,
+    public createdAt: string,
+    public updatedAt: string,
+    public isBlocked: boolean,
+    public isLogged: boolean,
     public points: number,
-    public user_type: string,
-    public seen_messages: boolean
+    public userType: string,
+    public seenMessages: boolean
   ) {
   }
 }
