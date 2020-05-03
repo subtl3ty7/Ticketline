e52896aedbd32b88cc482639f5656e4fe1078501
egedemirsoy <egedemirsoy@gmail.com>
2020-05-03 15:57:54 +0200

    Updated the repository method to order the results by tickets sold

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    List<Event> findAllByStartsAtAfter(LocalDateTime time);
+    List<Event> findAllByStartsAtAfterOrderByTicketsSoldDesc(LocalDateTime time);
 
 }
