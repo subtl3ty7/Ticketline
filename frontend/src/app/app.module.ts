a4eedfb8080a75b71baa171274f547a80e93a1ae
egedemirsoy <egedemirsoy@gmail.com>
2020-05-14 03:05:32 +0200

    Hotfix: Changed import statement

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -50,7 +50,7 @@ import { MyProfileTabsComponent } from './components/my-profile/my-profile-tabs/
 import { MyInfoTabComponent } from './components/my-profile/my-profile-tabs/my-info-tab/my-info-tab.component';
 import { EditProfileComponent } from './components/my-profile/my-profile-tabs/my-info-tab/edit-profile/edit-profile.component';
 import { DeleteAccountComponent } from './components/my-profile/my-profile-tabs/my-info-tab/delete-account/delete-account.component';
-import { TopEventsComponent } from './components/guest-home/top-events/top-events.component';
+import { TopEventsComponent} from './components/home/top-events/top-events.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { EventSearchComponent } from './components/event-search/event-search.component';
 import {MatPaginatorModule} from '@angular/material/paginator';
