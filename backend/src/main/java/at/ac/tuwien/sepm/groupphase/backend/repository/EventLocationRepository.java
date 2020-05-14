7ec03c9a88a80773a9d3602c86d101bb834b3b73
egedemirsoy <egedemirsoy@gmail.com>
2020-05-14 13:09:20 +0200

    Added method documentations

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventLocationRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventLocationRepository.java
@@ -1,7 +1,6 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.EventLocation;
-import at.ac.tuwien.sepm.groupphase.backend.entity.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +8,19 @@ import java.util.List;
 
 @Repository
 public interface EventLocationRepository extends JpaRepository<EventLocation, Long> {
+
+    /**
+     * Find an event location by id.
+     *
+     * @param id
+     * @return a single event location with id
+     */
     EventLocation findEventLocationById(Long id);
+
+    /**
+     * Find all event locations with no shows.
+     *
+     * @return a list of event locations with no shows.
+     */
     List<EventLocation> findAllByShowIdIsNull();
 }
