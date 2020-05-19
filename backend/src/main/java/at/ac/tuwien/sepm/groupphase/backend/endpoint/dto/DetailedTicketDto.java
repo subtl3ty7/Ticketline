70038efffd136fce7d8747a359321f01e0bbec01
Nejra <e11719704@student.tuwien.ac.at>
2020-05-19 16:21:38 +0200

    [Issue#84] Implemented builders for ticketdtos

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/DetailedTicketDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/DetailedTicketDto.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Seat;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Show;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Ticket;
 import lombok.*;
 
 import javax.persistence.*;
@@ -11,17 +12,62 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
-@ToString
-@NoArgsConstructor
 public class DetailedTicketDto {
     private Long ticketId;
     private String ticketCode;
     private boolean isPurchased;
+    private boolean isReserved;
     private LocalDateTime purchaseDate;
     private Seat seat;
     private String userCode;
     private Integer price;
     private Show show;
+
+    public static final class DetailedTicketDtoBuilder{
+        private Long ticketId;
+        private String ticketCode;
+        private boolean isPurchased;
+        private boolean isReserved;
+        private LocalDateTime purchaseDate;
+        private Seat seat;
+        private String userCode;
+        private Integer price;
+        private Show show;
+
+        public DetailedTicketDtoBuilder(Long ticketId, String ticketCode, boolean isPurchased, boolean isReserved,
+                                        LocalDateTime purchaseDate, Seat seat, String userCode, Integer price, Show show){
+            this.ticketId = ticketId;
+            this.ticketCode = ticketCode;
+            this.isPurchased = isPurchased;
+            this.isReserved = isReserved;
+            this.purchaseDate =  purchaseDate;
+            this.seat = seat;
+            this.userCode = userCode;
+            this.price = price;
+            this.show = show;
+        }
+
+        public static DetailedTicketDto.DetailedTicketDtoBuilder aDetailedTicketDto(Long ticketId, String ticketCode, boolean isPurchased,
+                                                                                    boolean isReserved, LocalDateTime purchaseDate, Seat seat, String userCode,
+                                                                                    Integer price, Show show){
+            return new DetailedTicketDto.DetailedTicketDtoBuilder(ticketId, ticketCode, isPurchased, isReserved, purchaseDate, seat, userCode, price, show);
+        }
+
+        public DetailedTicketDto build(){
+            DetailedTicketDto detailedTicketDto = new DetailedTicketDto();
+            detailedTicketDto.setTicketId(ticketId);
+            detailedTicketDto.setTicketCode(ticketCode);
+            detailedTicketDto.setPurchased(isPurchased);
+            detailedTicketDto.setReserved(isReserved);
+            detailedTicketDto.setPurchaseDate(purchaseDate);
+            detailedTicketDto.setSeat(seat);
+            detailedTicketDto.setUserCode(userCode);
+            detailedTicketDto.setPrice(price);
+            detailedTicketDto.setShow(show);
+            return detailedTicketDto;
+        }
+
+
+
+    }
 }
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/SimpleTicketDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/SimpleTicketDto.java
@@ -8,19 +8,61 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
-@ToString
-@NoArgsConstructor
 public class SimpleTicketDto {
     private Long ticketId;
     private String ticketCode;
     private boolean isPurchased;
+    private boolean isReserved;
     private LocalDateTime purchaseDate;
     private Long seatId;
     private String userCode;
     private Integer price;
     private Long showId;
+
+    public static final class SimpleTicketDtoBuilder {
+        private Long ticketId;
+        private String ticketCode;
+        private boolean isPurchased;
+        private boolean isReserved;
+        private LocalDateTime purchaseDate;
+        private Long seatId;
+        private String userCode;
+        private Integer price;
+        private Long showId;
+
+        public SimpleTicketDtoBuilder(Long ticketId, String ticketCode, boolean isPurchased, boolean isReserved,
+                                      LocalDateTime purchaseDate, Long seatId, String userCode, Integer price, Long showId) {
+            this.ticketId = ticketId;
+            this.ticketCode = ticketCode;
+            this.isPurchased = isPurchased;
+            this.isReserved = isReserved;
+            this.purchaseDate = purchaseDate;
+            this.seatId = seatId;
+            this.userCode = userCode;
+            this.price = price;
+            this.showId = showId;
+        }
+
+        public static SimpleTicketDto.SimpleTicketDtoBuilder aSimpleTicketDto(Long ticketId, String ticketCode, boolean isPurchased,
+                                                                              boolean isReserved, LocalDateTime purchaseDate, Long seatId, String userCode,
+                                                                              Integer price, Long showId) {
+            return new SimpleTicketDto.SimpleTicketDtoBuilder(ticketId, ticketCode, isPurchased, isReserved, purchaseDate, seatId, userCode, price, showId);
+        }
+
+        public SimpleTicketDto build() {
+            SimpleTicketDto simpleTicketDto = new SimpleTicketDto();
+            simpleTicketDto.setTicketId(ticketId);
+            simpleTicketDto.setTicketCode(ticketCode);
+            simpleTicketDto.setPurchased(isPurchased);
+            simpleTicketDto.setReserved(isReserved);
+            simpleTicketDto.setPurchaseDate(purchaseDate);
+            simpleTicketDto.setSeatId(seatId);
+            simpleTicketDto.setUserCode(userCode);
+            simpleTicketDto.setPrice(price);
+            simpleTicketDto.setShowId(showId);
+            return simpleTicketDto;
+        }
+    }
 
 
 }
