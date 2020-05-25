d9621d5b159ff7fdb34475aa4ced66c0315ea7a6
Aleksa Glamocanin <e11722602@student.tuwien.ac.at>
2020-05-25 13:45:22 +0200

    [Task #15] News, Artist and Reset Password Unit test implemented

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/basetest/TestData.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/basetest/TestData.java
@@ -58,7 +58,6 @@ public interface TestData {
     LocalDateTime END =
         LocalDateTime.of(2020, 11, 13, 18, 15, 0, 0);
     List<Integer> PRICES = List.of(30, 40);
-    List<String> ARTISTS = List.of("Artist1", "Artist2");
     List<Show> SHOWS = new ArrayList<>() {
         {
             add(Show.builder()
