32fb3a8acdbda4727d684fe7a701c02950c6ec30
Martin Nangole <m.nangole@gmx.at>
2020-05-26 02:53:09 +0200

    [Issue #63] Implementing frontend for event page; added new method to search-shared.ts for loading detailed events by name

--- frontend/src/app/components/search/event/event.component.ts
+++ frontend/src/app/components/search/event/event.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Artist} from '../../../dtos/artist';
+import {AuthService} from '../../../services/auth.service';
+import {ArtistService} from '../../../services/artist.service';
+import {ActivatedRoute} from '@angular/router';
+import {SearchShared} from '../search-shared';
+import {number} from '@amcharts/amcharts4/core';
 
 @Component({
   selector: 'app-event',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventComponent implements OnInit {
 
-  constructor() { }
+  name: string = '';
+  artistId: string;
+  constructor(public authService: AuthService, private artistService: ArtistService, private activatedRoute: ActivatedRoute, private searchShared: SearchShared) {
+
+  }
 
   ngOnInit(): void {
+    console.log('event search');
+    document.body.style.backgroundImage = null;
+    document.body.style.backgroundColor = '#DEDEDE';
+    this.name = sessionStorage.getItem('searchTerm');
+    console.log('event name: ' + this.name);
+    this.searchShared.getEventsByName(this.name);
   }
 
 }
