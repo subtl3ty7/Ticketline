976e83402620edef5f408323c82d6543469bf38d
e11713077 <e11713077@student.tuwien.ac.at>
2020-05-30 16:37:42 +0200

    [Issue#25] - implemented basic columns of Merchandise Entity

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Merchandise.java
@@ -0,0 +1,61 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import lombok.*;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "merchandise")
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@ToString
+public class Merchandise {
+
+    public Merchandise() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long merchandiseProductId;
+
+    @NotNull
+    @Size(min = 6, max = 6)
+    @Column(nullable = false, length = 6, name = "merchandise_code", unique = true)
+    private String merchandiseProductCode;
+
+    @NotNull
+    @Column(nullable = false, name = "product_name")
+    private String merchandiseProductName;
+
+    @ToString.Exclude
+    @NotNull
+    @Lob
+    @Column(nullable = false, name = "photo")
+    private String photo;
+
+    @NotNull
+    @Column(nullable = false, name = "stock_count")
+    private int stockCount;
+
+    @NotNull
+    @Column(nullable = false, name = "price")
+    private int price;
+
+
+    @NotNull
+    @Column(nullable = false, name = "premium_price")
+    private int premiumPrice;
+
+    @NotNull
+    @Column(nullable = false, name = "is_premium")
+    private boolean isPremium;
+
+
+
+}
