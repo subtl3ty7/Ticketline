40d1185e2502728a2c2fcdcb4d4293a89fde5169
egedemirsoy <egedemirsoy@gmail.com>
2020-06-01 15:49:24 +0200

    [Issue #100] Changing type of startsAt and endsAt to Date from string

--- frontend/src/app/dtos/simple-event.ts
+++ frontend/src/app/dtos/simple-event.ts
@@ -2,8 +2,8 @@ import {Show} from './show';
 
 export class SimpleEvent {
   public eventCode: string;
-  public startsAt: string;  // will be LocalDateTime in backend
-  public endsAt: string; // will be LocalDateTime in backend
+  public startsAt: Date;  // will be LocalDateTime in backend
+  public endsAt: Date; // will be LocalDateTime in backend
   public name: string;
   public description: string;
   public startPrice: number;
