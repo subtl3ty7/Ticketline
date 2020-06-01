a3ed6fae52d9ed6ef8438540c0fe031c2d1a8c95
egedemirsoy <egedemirsoy@gmail.com>
2020-06-01 15:57:55 +0200

    [Issue #100] Implementing news search and news overview (admin)

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/CustomNewsService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/CustomNewsService.java
@@ -94,14 +94,32 @@ public class CustomNewsService implements NewsService {
     public News findByNewsCode(String newsCode, Authentication auth) {
         AbstractUser user = userService.getAuthenticatedUser(auth);
         validator.validateUser(user).throwIfViolated();
-        Customer customer = (Customer) user;
         News news = newsRepository.findByNewsCode(newsCode);
         if (news==null) {
             throw new NotFoundException("Could not find this News entry");
         } else {
-            //if news entry was found, then mark the news entry as seen by the customer
-            this.markAsSeen(customer, news);
+            if(user instanceof Customer) {
+                //if news entry was found, then mark the news entry as seen by the customer
+                Customer customer = (Customer) user;
+                this.markAsSeen(customer, news);
+            }
         }
+        return news;
+    }
+
+    @Override
+    public List<News> findAll() {
+        return newsRepository.findAll();
+    }
+
+    @Override
+    public List<News> findSimpleNewsByParam(String newsCode,
+                                            String title,
+                                            String author,
+                                            LocalDateTime startRange,
+                                            LocalDateTime endRange) {
+
+        List<News> news =  newsRepository.findAllByNewsCodeContainingIgnoreCaseAndTitleContainingIgnoreCaseAndAuthorContainingIgnoreCaseAndPublishedAtBetween(newsCode, title, author, startRange, endRange);
         return news;
     }
 
