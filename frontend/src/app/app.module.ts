5e7315d6292caf62b7f27198cd3204f1ab0930f3
Martin Nangole <m.nangole@gmx.at>
2020-06-05 00:36:00 +0200

    Merging develop (not compiling)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -187,7 +187,7 @@ import {MatDialogModule} from '@angular/material/dialog';
     PaymentDoneComponent,
     MyInvoiceComponent,
     EventAdvancedComponent,
-    SpinnerComponent
+    SpinnerComponent,
     MyInvoiceComponent,
     NewsTabComponent,
     NewsDetailsContainerComponent,
