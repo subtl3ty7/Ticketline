f651a24e1c67361c51f050f4dfbadfa516330b3e
Martin Nangole <m.nangole@gmx.at>
2020-06-12 01:23:04 +0200

    [Issue #124] Reviewing translated error messages

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/util/ValidationMessage.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/util/ValidationMessage.java
@@ -20,48 +20,48 @@ public class ValidationMessage {
         {
             //user
             put("user_id", "Id muss eindeutig und nicht null sein.");
-            put("user_userCode", "User Code muss 6 Zeichnen lang sein.");
-            put("user_email","Email darf nicht leer oder länger als 100 Zeichnen sein.");
-            put("user_firstName",  "Vorname darf nicht leer oder länger als 30 Zeichnen sein.");
-            put("user_lastName","Nachname darf nicht leer oder länger als 30 Zeichnen sein.");
-            put("user_password", "Passwort muss zwischen 8 und 30 Zeichnen lang sein.");
+            put("user_userCode", "User Code muss 6 Zeichen lang sein.");
+            put("user_email","E-Mail darf nicht leer oder länger als 100 Zeichen sein.");
+            put("user_firstName",  "Vorname darf nicht leer oder länger als 30 Zeichen sein.");
+            put("user_lastName","Nachname darf nicht leer oder länger als 30 Zeichen sein.");
+            put("user_password", "Passwort muss zwischen 8 und 30 Zeichen lang sein.");
             put("user_createdAt", "Created-At Feld darf nicht leer sein.");
             put("user_updatedAt", "Updated-At Feld darf nicht leer sein.");
             put("user_birthday","Geburtsdatum Feld darf nicht leer sein.");
-            put("points_zero", "Punktestand muss bei der Registirerung 0 sein.");
+            put("points_zero", "Punktestand muss bei der Registrierung 0 sein.");
             put("password_encoded", "Passwort muss bCrypt codiert sein.");
-            put("birthday_16yo", "UserIn muss mindestens 16 Jahr alt sein.");
+            put("birthday_16yo", "BenutzerIn muss mindestens 16 Jahre alt sein.");
             put("userCode_unique", "User Code ist bereits in der Datenbank vorhanden.");
             put("userCode_notNull", "User Code darf nicht null sein.");
             put("isLogged_false", "Logged-In Feld darf nicht 'true' bei der Registrierung sein.");
             put("isBlocked_false", "Is-Blocked Feld darf nicht 'true' bei der Registrierung sein.");
-            put("email_notNull", "EMail darf nicht null sein.");
-            put("email_unique", "Email ist beretis in der Datenbank vorhanden.");
-            put("email_valid", "EMail sieht nicht wie eine gültige Email-Adresse aus.");
-            put("user_isLogged", "User muss eingeloggt sein.");
-            put("user_exists", "User existiert nicht.");
-            put("user_notAdmin", "User darf nicht Admin sein.");
-            put("user_keepsRole", "User darf nicht die Rolle wechseln.");
-            put("user_isCustomer", "User muss Kunde/Kundin sein.");
-            put("user_isUnblocked", "User muss entsperrt sein, um gesperrt zu werden.");
-            put("user_iBlocked", "User muss gesperrt sein, um entsperrt zu werden.");
-            put("user_isSelf", "User ist nicht selbst.");
-            put("user_isSelf_or_auth_isAdmin", "Entweder ist user nicht selbst oder User hat nicht Recht für diese Aktion.");
+            put("email_notNull", "E-Mail darf nicht null sein.");
+            put("email_unique", "E-Mail ist bereits in der Datenbank vorhanden.");
+            put("email_valid", "E-Mail scheint keine gültige Email-Adresse zu sein.");
+            put("user_isLogged", "BenutzerIn muss eingeloggt sein.");
+            put("user_exists", "BenutzerIn existiert nicht.");
+            put("user_notAdmin", "BenutzerIn darf kein/e AdministratorIn sein.");
+            put("user_keepsRole", "BenutzerIn darf nicht die Rolle wechseln.");
+            put("user_isCustomer", "BenutzerIn muss Kunde/Kundin sein.");
+            put("user_isUnblocked", "BenutzerIn muss entsperrt sein, um gesperrt zu werden.");
+            put("user_iBlocked", "BenutzerIn muss gesperrt sein, um entsperrt zu werden.");
+            put("user_isSelf", "Unerwartete/r BenutzerIn.");
+            put("user_isSelf_or_auth_isAdmin", "BenutzerIn ist unerwartet in diesem Kontext oder hat nicht die Rechte für diese Aktion.");
 
 
             //seat
             put("seat_seatRow", "Reihe darf nicht leer sein.");
-            put("seat_seatColumn", "Säule darf nicht leer sein.");
+            put("seat_seatColumn", "Spalte darf nicht leer sein.");
 
             //section
-            put("section_sectionName", "Name darf nicht leer oder länger als 100 Zeichnen sein.");
-            put("section_sectionDescription", "Beschreibung darf nicht leer oder länger als 1000 Zeichnen sein.");
+            put("section_sectionName", "Name darf nicht leer oder länger als 100 Zeichen sein.");
+            put("section_sectionDescription", "Beschreibung darf nicht leer oder länger als 1000 Zeichen sein.");
             put("section_seats", "Sitzliste darf nicht null sein.");
-            put("section_capacity", "Die Sektionsskapazität darf nicht kleiner sein als die Anzahl der Sektionssplätze.");
+            put("section_capacity", "Die Sektionskapazität darf nicht kleiner sein als die Anzahl der Sektionsplätze.");
 
             //eventLocation
-            put("eventLocation_eventLocationName", "Name darf nicht leer oder länger als 100 Zeichnen sein.");
-            put("eventLocation_eventLocationDescription", "Beschreibung darf nicht leer oder länger als 1000 Zeichnen sein.");
+            put("eventLocation_eventLocationName", "Name darf nicht leer oder länger als 100 Zeichen sein.");
+            put("eventLocation_eventLocationDescription", "Beschreibung darf nicht leer oder länger als 1000 Zeichen sein.");
             put("eventLocation_sections", "Sektionsliste darf nicht null sein.");
             put("eventLocation_capacity", "Veranstaltungsortskapazität darf nicht kleiner sein als die Kapazitätssumme seiner Sektionen.");
 
@@ -73,18 +73,18 @@ public class ValidationMessage {
             put("eventLocation_onlyOne", "In der Liste ist nur ein Veranstaltungsort zulässig.");
             put("eventLocation_exists", "Veranstaltungsort existiert nicht in der Datenbank.");
             put("eventLocation_unassigned", "Der Veranstaltungsort ist bereits einer Aufführung zugeordnet.");
-            put("show_idNull", "Aufführung ID sollte null sein.");
+            put("show_idNull", "Aufführungs-ID sollte null sein.");
 
             //event
-            put("event_eventCode", "Event Code darf nicht leer sein und muss 6 Zeichnen lang sein.");
-            put("event_name", "Name darf nicht leer oder länger als 100 Zeichnen sein.");
-            put("event_description", "Beschreibung darf nicht leer oder länger als 1000 Zeichnen sein.");
-            put("event_category", "Kategorie darf nicht leer oder länger als 100 Zeichnen sein.");
-            put("event_type", "Typ darf nicht leer oder länger als 100 Zeichnen sein.");
+            put("event_eventCode", "Event Code darf nicht leer und muss 6 Zeichen lang sein.");
+            put("event_name", "Name darf nicht leer oder länger als 100 Zeichen sein.");
+            put("event_description", "Beschreibung darf nicht leer oder länger als 1000 Zeichen sein.");
+            put("event_category", "Kategorie darf nicht leer oder länger als 100 Zeichen sein.");
+            put("event_type", "Typ darf nicht leer oder länger als 100 Zeichen sein.");
             put("event_startsAt", "Veranstaltung braucht start-At Feld.");
             put("event_endsAt", "Veranstaltung braucht ends-At Feld.");
             put("event_photo", "Veranstaltung muss ein Foto beinhalten.");
-            put("event_shows", "Aufführungliste darf nicht null sein.");
+            put("event_shows", "Aufführungsliste darf nicht null sein.");
             put("event_artists", "KünstlerInnenliste darf nicht null sein.");
             put("event_prices", "Preisliste darf nicht null sein.");
             put("eventCode_unique", "Veranstaltungscode ist bereits in der Datenbank vorhanden.");
@@ -93,41 +93,41 @@ public class ValidationMessage {
             //ticket
             put("ticketCode_unique", "Ticket code ist schon in der Datenbank vorhanden.");
             put("userCode_exists", "User wurde nicht in der Datenbank gefunden.");
-            put("show_exists", "SAufführung wurde nicht in der Datenbank gefunden.");
-            put("seat_notFree", "Platz ist bereits besetzt.");
+            put("show_exists", "Aufführung wurde nicht in der Datenbank gefunden.");
+            put("seat_notFree", "Platz ist bereits reserviert.");
             put("seat_exists", "Platz wurde nicht in der Datenbank gefunden.");
             put("tickets_sold", "Diese Aufführung ist ausverkauft.");
             put("no_tickets", "Dieser User hat keine Tickets.");
             put("ticket_exists", "Dieses Ticket ist schon in der Datenbank gespeichert.");
-            put("admin_purchase", "Admin kann nicht die Tickets kaufen oder reservieren.");
+            put("admin_purchase", "AdministratorIn kann nicht die Tickets kaufen oder reservieren.");
             put("price_exists", "Preis muss eingegeben werden.");
             put("price_zero", "Preis darf nicht 0 sein.");
-            put("seat_zero", "Platz id darf nicht 0 sein.");
-            put("show_zero", "Aufführung id darf nicht 0 sein.");
-            put("seat_null", "Platz id darf nicht null sein.");
-            put("show_null", "Aufführung id darf nicht null sein.");
+            put("seat_zero", "Platz-ID darf nicht 0 sein.");
+            put("show_zero", "Aufführungs-ID darf nicht 0 sein.");
+            put("seat_null", "Platz-ID darf nicht null sein.");
+            put("show_null", "Aufführungs-ID darf nicht null sein.");
             put("price_null", "Preis darf nicht null sein.");
             put("ticket_notExist", "Ticket wurde nicht in der Datenbank gefunden.");
             put("ticket_purchased", "Ticket wurde noch nicht gekauft.");
             put("ticket_reserved", "Ticket wurde schon gekauft.");
 
             //news
-            put("newsCode_unique", "News code ist schon in der Datenbank vorhanden.");
-            put("news_newsCode", "News code muss 6 Zeichen lang sein.");
-            put("news_title", "News Titel darf nicht leer oder länger als 100 Zeichnen sein.");
+            put("newsCode_unique", "News-Code ist schon in der Datenbank vorhanden.");
+            put("news_newsCode", "News-Code muss 6 Zeichen lang sein.");
+            put("news_title", "News-Titel darf nicht leer oder länger als 100 Zeichen sein.");
             put("news_publishedAt", "Veröffentlichungsdatum muss vorhanden sein.");
             put("news_stopsBeingRelevantAt", "Relevanzdatum muss vorhanden sein.");
-            put("news_summary", "News Zusammenfassung darf nicht leer oder länger als 1000 Zeichnen sein.");
-            put("news_text", "News text darf nicht leer oder länger als 100.000 Zeichnen sein.");
-            put("news_author", "News autor muss vorhanden und nicht länger als 100 Zeichnen sein.");
-            put("news_photo", "News Foto muss vorhanden sein.");
+            put("news_summary", "News-Zusammenfassung darf nicht leer oder länger als 1000 Zeichen sein.");
+            put("news_text", "News-Text darf nicht leer oder länger als 100.000 Zeichen sein.");
+            put("news_author", "News-Autor muss vorhanden und nicht länger als 100 Zeichen sein.");
+            put("news_photo", "News-Foto muss vorhanden sein.");
             put("seenBy_notNull", "SeenBy Liste darf nicht null sein.");
 
             //merchandise
-            put("merchandise_premium", "Merchandise ist keine Prämie und kann nicht mit Prämienpunkten gekauft werden.");
-            put("merchandise_outOfStock", "Merchandise ist ausverkauft.");
-            put("merchandise_exists", "Merchandise wurde in der Datenbank nicht gefunden.");
-            put("user_noPoints", "User hat nicht genügend Premium-Punkte, um dieses Produkt zu kaufen.");
+            put("merchandise_premium", "Produkt ist kein Prämienprodukt und kann daher nicht mit Prämienpunkten gekauft werden.");
+            put("merchandise_outOfStock", "Produkt ist ausverkauft.");
+            put("merchandise_exists", "Produkt wurde in der Datenbank nicht gefunden.");
+            put("user_noPoints", "BenutzerIn hat nicht genügend Prämienpunkte, um dieses Produkt zu kaufen.");
         }
     };
 
