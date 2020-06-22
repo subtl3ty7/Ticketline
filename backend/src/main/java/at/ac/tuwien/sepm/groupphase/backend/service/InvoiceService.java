3819f638a8b9d10b2976897b753718b47615aea5
e11713077 <e11713077@student.tuwien.ac.at>
2020-06-22 14:03:19 +0200

    [Issue #110] - added JavaDoc for InvoiceService

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/InvoiceService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/InvoiceService.java
@@ -10,8 +10,31 @@ import java.util.List;
 
 public interface InvoiceService {
 
+    /**
+     * Create the invoice for the ticket(s)
+     *
+     * @param tickets - ticket(s) for which an invoice needs to be created
+     * @param type - determines if the invoice is for the purchase or the cancellation
+     * @param generated_st - the date and time when the invoice was generated
+     * @return - an Invoice for the given bought ticket(s)
+     */
     Invoice createTicketInvoice(List<Ticket> tickets, String type, LocalDateTime generated_st);
+
+    /**
+     *
+     * @param merchandise - merchandise product for which an invoice needs to be created
+     * @param userCode - code of the user that has bought this merchandise product
+     * @param pay - determines if the user paid with premium points or with money
+     * @return - an Invoice for the given and bought merchandise product
+     */
     Invoice createMerchandiseInvoice(Merchandise merchandise, String userCode, String pay);
+
+    /**
+     * Finds all invoices of an user.
+     *
+     * @param userCode - user code to look for
+     * @return - list of all invoices that have the same userCode
+     */
     List<Invoice> allInvoicesOfUser(String userCode);
 
 }
