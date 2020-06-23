66c2d5ee4ad7aff6112cd70ef688e208eee2e6fb
e11713077 <e11713077@student.tuwien.ac.at>
2020-06-23 21:14:18 +0200

    [Issue #110] - added JavaDoc for NewsService

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/NewsService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/NewsService.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface NewsService {
 
     /**
-     * Find the latest unseen news
+     * Find the latest unseen news articles
      *
      * @param auth - stores user authentication data (for example - email)
      * @param limit - how many news should be displayed
@@ -17,8 +17,23 @@ public interface NewsService {
      */
     List<News> findLatestUnseen(Authentication auth, Integer limit);
 
+    /**
+     * Find the news articles that the user already saw
+     *
+     * @param page - index from the page
+     * @param size - number of news entries on the same page
+     * @param auth - stores user authentication data (for example - email)
+     * @return - a list of news articles that the customer already saw
+     */
     List<News> findSeenNews(Integer page, Integer size, Authentication auth);
 
+    /**
+     * Finds the latest news (the news that are recently added)
+     *
+     * @param page - index from the page
+     * @param size - number of news entries on the same page
+     * @return - a list of recently added news articles
+     */
     List<News> findLatest(Integer page, Integer size);
 
     /**
@@ -38,8 +53,25 @@ public interface NewsService {
      */
     News findByNewsCode(String eventCode, Authentication auth);
 
+    /**
+     * Find all news articles
+     *
+     * @param size - number of news entries on the same page
+     * @return - a list of all the news articles
+     */
     List<News> findAll(int size);
 
+    /**
+     * Find simple news according to newsCode, title, author, startRangeDate and endRangeDate
+     *
+     * @param newsCode - a code of the news to look for
+     * @param title - title of the news article to look for
+     * @param author - author of the news article to look for
+     * @param startRangeDate - start range date to look for
+     * @param endRangeDate - end range date to look for
+     * @param size - number of news entries on the same page
+     * @return - a list of news articles that match the given criteria
+     */
     List<News> findSimpleNewsByParam(String newsCode, String title, String author, LocalDateTime startRangeDate, LocalDateTime endRangeDate, int size);
 }
 
