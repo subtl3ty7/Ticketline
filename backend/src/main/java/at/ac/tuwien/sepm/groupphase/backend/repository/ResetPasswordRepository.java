667777ee110b3339cb7b3c0478b258b94a7e94b0
egedemirsoy <egedemirsoy@gmail.com>
2020-06-23 23:52:53 +0200

    [Issue #110] Writing missing Javadoc for ResetPassword Interfaces

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ResetPasswordRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ResetPasswordRepository.java
@@ -11,24 +11,26 @@ import java.util.List;
 public interface ResetPasswordRepository extends JpaRepository<ResetPassword, Long> {
 
     /**
-     *
+     * Find the Resetpassword object which contains email and reset password code
+     *  associated with an email
      *
      * @param email - email to look for
-     * @return
+     * @return - Resetpassword object which contains email and reset password code
      */
     ResetPassword findByEmail(String email);
 
     /**
-     *
+     * Find the Resetpassword object which contains email and reset password code
+     * associated with a reset password code.
      * @param resetPasswordCode - reset password code to look for
-     * @return
+     * @return - Resetpassword object which contains email and reset password code
      */
     ResetPassword findByResetPasswordCode(String resetPasswordCode);
 
     /**
-     *
+     * Find the ResetPassword objects which are created before a specific time
      * @param localDateTime - local date time to look for
-     * @return
+     * @return - list of ResetPassword objects that are created before the given time
      */
     List<ResetPassword> findAllByCreatedAtBefore(LocalDateTime localDateTime);
 
